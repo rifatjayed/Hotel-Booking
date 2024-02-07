e883e32a63b63f11ba4e7985db76e425d5fe863f
Rifat Jayed <rifat.jayed2000@gmail.com>
2024-02-07 11:53:30 +0600

    Search box complete

--- src/Components/SearchItem/SearchItem.jsx
+++ src/Components/SearchItem/SearchItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './SearchItem.css'
 const SearchItem = () => {
     return (
-        <div>
-            
+        <div className='searchItem'>
+            {/* <img src="https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1" alt="" className="siImg" /> */}
         </div>
     );
 };
--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -5,46 +5,76 @@ import Header from '../../Components/Header/Header';
 import { useLocation } from 'react-router-dom';
 import { format } from 'date-fns';
 import { DateRange } from 'react-date-range';
+import SearchItem from '../../Components/SearchItem/SearchItem';
 
 const List = () => {
 
-const location= useLocation()
-const [destination, setDestination]= useState(location.state.destination)
-const [date, setDate]= useState(location.state.date)
-const [openDate, setOpenDate]= useState(false)
-const [options, setOptions]= useState(location.state.options)
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Header type="List"></Header>
-            <div className="listContainer">
-                <div className="listWrapper">
-                    <div className="listSearch">
-                      <h1 className="lsTitle">
-                        Search
-                      </h1>
-                      <div className="lsItem">
-                        <label >Destination</label>
-                      <input placeholder={destination} type="text" />
-                      </div>
-                      <div className="lsItem">
-                        <label >Check-In date</label>
-                     <span onClick={()=>setOpenDate(!openDate)}>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
-                    {openDate &&
-                (
-                  <DateRange onChange={item=>setDate([item.selection])}
-                  minDate={new Date()}
-                  ranges={date}></DateRange>
-                )
-                    }
-                      </div>
-                    
-                    </div>
-                    <div className="lisResult"></div>
-                </div>
+  const location = useLocation()
+  const [destination, setDestination] = useState(location.state.destination)
+  const [date, setDate] = useState(location.state.date)
+  const [openDate, setOpenDate] = useState(false)
+  const [options, setOptions] = useState(location.state.options)
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Header type="List"></Header>
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">
+              Search
+            </h1>
+            <div className="lsItem">
+              <label >Destination</label>
+              <input placeholder={destination} type="text" />
             </div>
+            <div className="lsItem">
+              <label >Check-In date</label>
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(date[0].endDate, "dd/MM/yyyy")}`}</span>
+              {openDate &&
+                (
+                  <DateRange onChange={item => setDate([item.selection])}
+                    minDate={new Date()}
+                    ranges={date}></DateRange>
+                )
+              }
+            </div>
+            <div className="lsItem">
+              <label>Options</label>
+              <div className="lsOption">
+
+             
+              <div className="lsOptionItem">
+                <div className="lsOptionText">Min Price <small>Per Night</small></div>
+                <input type="number" className="lsOptionInput" />
+              </div>
+              <div className="lsOptionItem">
+                <div className="lsOptionText">Max Price <small>Per Night</small></div>
+                <input type="number" className="lsOptionInput" />
+              </div>
+              <div className="lsOptionItem">
+                <div className="lsOptionText">Adult </div>
+                <input type="number" min={1} className="lsOptionInput" placeholder={options.adult}/>
+              </div>
+              <div className="lsOptionItem">
+                <div className="lsOptionText">Children</div>
+                <input type="number" min={0} className="lsOptionInput" placeholder={options.children}/>
+              </div>
+              <div className="lsOptionItem">
+                <div className="lsOptionText">Room</div>
+                <input type="number" min={1} className="lsOptionInput" placeholder={options.room}/>
+              </div>
+            </div>
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="lisResult">
+            <SearchItem></SearchItem>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default List;
