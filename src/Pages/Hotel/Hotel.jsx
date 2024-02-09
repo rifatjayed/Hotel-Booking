edc512cd9a323edd44a5acc587c51c96002c79f3
Rifat Jayed <rifat.jayed2000@gmail.com>
2024-02-09 23:57:16 +0600

    hotel section start

--- src/Pages/Hotel/Hotel.jsx
+++ src/Pages/Hotel/Hotel.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Header from '../../Components/Header/Header';
+import { FaLocationDot } from "react-icons/fa6";
+
+
 
 const Hotel = () => {
     return (
         <div>
-            <h1>hotel</h1>
+            <Navbar></Navbar>
+            <Header type="List"></Header>
+            <div className="hotelContainer">
+                <div className="hotelWrapper">
+                    <h1 className="hotelTitle">Grand Hotel</h1>
+                    <div className="hotelAddress">
+                        <FaLocationDot />
+                        <span>Elton st 125 New York</span>
+                    </div>
+                    <span className="hotelDistance">
+                        Excellent Location - 500m from center
+                    </span>
+                    <span className="hotelPriceHighlight">
+                        Book a stay over $114 at this property and get a free airport taxi
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
